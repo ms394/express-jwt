e9b0ff19b37981d53d7c2fe01d923d4dcd1a050d
ms394 <mohsinsajan394@gmail.com>
2022-12-15 12:00:42 +0530

    Passport Auth Implementation

--- config/passport.js
+++ config/passport.js
@@ -1,13 +1,13 @@
 const passport = require("passport");
+const { Strategy } = require("passport-jwt");
 const JWTStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const queries = require("../queries");
-const bcrypt = require("bcrypt");
 
 const options = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.TOKEN_SECRET,
-  algorithms: ["RS256"],
+  //   algorithms: ["RS256"], --> Can be used only when we have 64bit private keys
 };
 
 const verifyCallback = async (jwt_payload, done) => {
@@ -15,11 +15,12 @@ const verifyCallback = async (jwt_payload, done) => {
     const { email } = jwt_payload;
     const user = await queries.getUserByEmail(email);
     if (!user) {
-      return done(null, false);
+      return done(null, false, "Please login again.");
     } else {
       return done(null, user);
     }
   } catch (err) {
+    console.log(err);
     return done(err, false);
   }
 };
